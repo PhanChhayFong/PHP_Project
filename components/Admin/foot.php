1c6c3c7782a7148e4483153af222d0c42c284664
ReactGroupWeb <react.project.2023@gmail.com>
2023-04-23 10:20:37 +0700

    PHP_Project

--- components/Admin/foot.php
+++ components/Admin/foot.php
@@ -6,6 +6,7 @@
 </div>
 </div>
 <!-- container-scroller -->
+
 <!-- bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
@@ -14,14 +15,19 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
   integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 <script src="../../assets/admin/vendors/js/vendor.bundle.base.js"></script>
-<!-- endinject -->
+<!-- End bootstrap -->
+
 <!-- Plugin js for this page -->
 <script src="../../assets/admin/vendors/chart.js/Chart.min.js"></script>
 <script src="../../assets/admin/vendors/jvectormap/jquery-jvectormap.min.js"></script>
 <script src="../../assets/admin/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- End plugin js for this page -->
+
+<!-- inject:js -->
+<script src="../../assets/admin/js/off-canvas.js"></script>
 <script src="../../assets/admin/js/misc.js"></script>
 <!-- endinject -->
+
 <!-- Custom js for this page -->
 <script src="../../assets/admin/js/dashboard.js"></script>
 <!-- End custom js for this page -->
