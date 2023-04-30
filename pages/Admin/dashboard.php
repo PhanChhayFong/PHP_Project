daeb406b6854c5339a40b76ff1fc8fdcf47146d6
ReactGroupWeb <react.project.2023@gmail.com>
2023-04-30 19:47:28 +0700

    PHP_Project

--- pages/Admin/dashboard.php
+++ pages/Admin/dashboard.php
@@ -2,7 +2,6 @@
 <!-- Dashboard Component -->
 <?php require 'components/Admin/dashboard/sale-growth.php'; ?>
 <?php require 'components/Admin/dashboard/transaction.php'; ?>
-<?php require 'components/Admin/dashboard/revenue.php'; ?>
 <?php require 'components/Admin/dashboard/order-history.php'; ?>
 <?php require 'components/Admin/dashboard/property.php'; ?>
 <?php require 'components/Admin/dashboard/visited-country.php'; ?>
