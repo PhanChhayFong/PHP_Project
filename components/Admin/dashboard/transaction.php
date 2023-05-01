011ff2e82ba9e46ea1719b23f73b35db13cbd761
ReactGroupWeb <react.project.2023@gmail.com>
2023-05-01 09:59:32 +0700

    PHP_Project

--- components/Admin/dashboard/transaction.php
+++ components/Admin/dashboard/transaction.php
@@ -1,48 +1,88 @@
 <?php
-  date_default_timezone_set('Asia/Phnom_Penh');
-  $current_time = date('D d M Y, h:i A', time());
-  
-  include_once "./DB/dbConnection.php";
-  include_once "./DB/dbClass.php";
-  $dbClass = new dbClass();
+date_default_timezone_set('Asia/Phnom_Penh');
+$current_time = date('D d M Y, h:i A', time());
 
-  $table_transaction = "tb_transaction as ts";
-  $table_order = "tb_order as ord";
+include_once "./DB/dbConnection.php";
+include_once "./DB/dbClass.php";
+$dbClass = new dbClass();
 
-  $field = "ts.id, ts.od_id, ts.tmode, ord.od_id, ord.totalPrice";
-  $condition = "";
-  $order = "";
+$table_transaction = "tb_transaction as ts";
+$table_order = "tb_order as ord";
 
-  $join_conditon = "ord.od_id = ts.od_id";
-  $table_join = $table_transaction . " INNER JOIN " . $table_order . " ON " . $join_conditon;
-  $transactions = $dbClass->dbSelect($table_join, $field, $condition, $order);
+$field = "ts.id, ts.od_id, ts.tmode, ord.od_id, ord.totalPrice";
+$condition = "";
+$order = "";
 
-  $total_paid_by_cash = 0;
-  $total_paid_by_card = 0;
-  $total_paid = 0;
+$join_conditon = "ord.od_id = ts.od_id";
+$table_join = $table_transaction . " INNER JOIN " . $table_order . " ON " . $join_conditon;
+$transactions = $dbClass->dbSelect($table_join, $field, $condition, $order);
 
-  if(!empty($transactions)){
-    foreach($transactions as $transaction){
-      if($transaction['tmode'] == "cash_on_delivery"){
-        $total_paid_by_cash += $transaction['totalPrice'];
-      }
-      else if($transaction['tmode'] == "card"){
-        $total_paid_by_card += $transaction['totalPrice'];
-      }
+$total_paid_by_cash = 0;
+$total_paid_by_card = 0;
+$total_paid = 0;
+
+if (!empty($transactions)) {
+  foreach ($transactions as $transaction) {
+    if ($transaction['tmode'] == "cash_on_delivery") {
+      $total_paid_by_cash += $transaction['totalPrice'];
+    } else if ($transaction['tmode'] == "card") {
+      $total_paid_by_card += $transaction['totalPrice'];
     }
   }
+}
 
-  $total_paid = $total_paid_by_cash + $total_paid_by_card;
+$total_paid = $total_paid_by_cash + $total_paid_by_card;
 
-  $_SESSION['total_paid'] = $total_paid;
-  $_SESSION['total_paid_by_cash'] = $total_paid_by_cash;
-  $_SESSION['total_paid_by_card'] = $total_paid_by_card;
-  
+$_SESSION['total_paid'] = $total_paid;
+$_SESSION['total_paid_by_cash'] = $total_paid_by_cash;
+$_SESSION['total_paid_by_card'] = $total_paid_by_card;
+
+
+$heading_user = "User";
+$heading_company = "Company";
+
+// User
+$table_user = "tb_user";
+
+$field = "*";
+$condition = "us_isAdmin != 0";
+
+$users = $dbClass->dbSelect($table_user, $field, $condition);
+
+// Company
+$table_company = "tb_company";
+$field = "*";
+$condition = "cp_id = 1";
+
+$company = $dbClass->dbSelectOne($table_company, $field, $condition);
 ?>
 
-
 <div class="row">
-  <div class="col-md-4 grid-margin stretch-card">
+  <div class="col-md-12 col-xl-4 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex flex-row justify-content-between">
+          <h4 class="card-title">Company</h4>
+        </div>
+        <div class="preview-list text-center">
+
+          <img src="../../../assets/images/<?= strtolower($heading_company) ?>/<?= $company['cp_logo']; ?>"
+            class="card-img-top company-logo mb-4" width="25">
+          <h3 class="card-title fw-bold">
+            <?= $company['cp_name'] ?>
+          </h3>
+          <p class="card-text"><a href="mailto:<?= $company['cp_email'] ?>" class="text-decoration-none text-light"><?= $company['cp_email'] ?></a></p>
+          <p class="card-text"><a href="tel:<?= $company['cp_phone'] ?>" class="text-decoration-none text-light"><?= $company['cp_phone'] ?></a></p>
+          <p class="card-text">
+            <?= $company['cp_address'] ?>
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-6 col-xl-4 grid-margin stretch-card">
     <div class="card">
       <div class="card-body">
         <h4 class="card-title">Transaction History</h4>
@@ -50,125 +90,69 @@
         <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
           <div class="text-md-center text-xl-left">
             <h6 class="mb-1">Payment on Cash Delivery</h6>
-            <p class="text-muted mb-0"><?= $current_time; ?></p>
+            <p class="text-muted mb-0">
+              <?= $current_time; ?>
+            </p>
           </div>
           <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-            <h6 class="font-weight-bold mb-0">$<?= number_format($total_paid_by_cash, 2) ?></h6>
+            <h6 class="font-weight-bold mb-0">$
+              <?= number_format($total_paid_by_cash, 2) ?>
+            </h6>
           </div>
         </div>
         <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
           <div class="text-md-center text-xl-left">
             <h6 class="mb-1">Tranfer to Stripe</h6>
-            <p class="text-muted mb-0"><?= $current_time; ?></p>
+            <p class="text-muted mb-0">
+              <?= $current_time; ?>
+            </p>
           </div>
           <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-            <h6 class="font-weight-bold mb-0">$<?= number_format($total_paid_by_card, 2) ?></h6>
+            <h6 class="font-weight-bold mb-0">$
+              <?= number_format($total_paid_by_card, 2) ?>
+            </h6>
           </div>
         </div>
       </div>
     </div>
   </div>
 
-
-
-
-  <div class="col-md-8 grid-margin stretch-card">
+  <div class="col-md-6 col-xl-4 grid-margin stretch-card">
     <div class="card">
       <div class="card-body">
-        <div class="d-flex flex-row justify-content-between">
-          <h4 class="card-title mb-1">Open Projects</h4>
-          <p class="text-muted mb-1">Your data status</p>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="preview-list">
-              <div class="preview-item border-bottom">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-primary">
-                    <i class="fa fa-file-text" aria-hidden="true"></i>
+        <h4 class="card-title">Team Members</h4>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <?php
+            if (!empty($users)) {
+              $index = 1;
+              foreach ($users as $user) {
+                ?>
+                <div class="carousel-item <?= $index == 1 ? "active" : "" ?>">
+                  <div class="card card-carousel py-0 mx-auto">
+                    <img src="../../../assets/images/<?= strtolower($heading_user) ?>/<?= $user['us_image']; ?>"
+                      class="card-img-top" style="max-height:350px;">
+                    <div class="card-body text-center py-0 mt-3">
+                      <h5 class="card-title fw-bold">
+                        <?= $user['us_name'] ?>
+                      </h5>
+                    </div>
                   </div>
                 </div>
-                <div class="preview-item-content d-sm-flex flex-grow">
-                  <div class="flex-grow">
-                    <h6 class="preview-subject">Admin dashboard design</h6>
-                    <p class="text-muted mb-0">Broadcast web app mockup</p>
-                  </div>
-                  <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                    <p class="text-muted">15 minutes ago</p>
-                    <p class="text-muted mb-0">30 tasks, 5 issues </p>
-                  </div>
-                </div>
-              </div>
-              <div class="preview-item border-bottom">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-success">
-                    <i class="fa fa-cloud-download"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content d-sm-flex flex-grow">
-                  <div class="flex-grow">
-                    <h6 class="preview-subject">Wordpress Development</h6>
-                    <p class="text-muted mb-0">Upload new design</p>
-                  </div>
-                  <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                    <p class="text-muted">1 hour ago</p>
-                    <p class="text-muted mb-0">23 tasks, 5 issues </p>
-                  </div>
-                </div>
-              </div>
-              <div class="preview-item border-bottom">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-info">
-                    <i class="fa fa-clock"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content d-sm-flex flex-grow">
-                  <div class="flex-grow">
-                    <h6 class="preview-subject">Project meeting</h6>
-                    <p class="text-muted mb-0">New project discussion</p>
-                  </div>
-                  <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                    <p class="text-muted">35 minutes ago</p>
-                    <p class="text-muted mb-0">15 tasks, 2 issues</p>
-                  </div>
-                </div>
-              </div>
-              <div class="preview-item border-bottom">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-danger">
-                    <i class="fa fa-envelope"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content d-sm-flex flex-grow">
-                  <div class="flex-grow">
-                    <h6 class="preview-subject">Broadcast Mail</h6>
-                    <p class="text-muted mb-0">Sent release details to team</p>
-                  </div>
-                  <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                    <p class="text-muted">55 minutes ago</p>
-                    <p class="text-muted mb-0">35 tasks, 7 issues </p>
-                  </div>
-                </div>
-              </div>
-              <div class="preview-item">
-                <div class="preview-thumbnail">
-                  <div class="preview-icon bg-warning">
-                    <i class="fa fa-chart-pie"></i>
-                  </div>
-                </div>
-                <div class="preview-item-content d-sm-flex flex-grow">
-                  <div class="flex-grow">
-                    <h6 class="preview-subject">UI Design</h6>
-                    <p class="text-muted mb-0">New application planning</p>
-                  </div>
-                  <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                    <p class="text-muted">50 minutes ago</p>
-                    <p class="text-muted mb-0">27 tasks, 4 issues </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+                <?php
+                $index++;
+              }
+            }
+            ?>
           </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
     </div>
--- pages/Admin/dashboard.php
+++ pages/Admin/dashboard.php
@@ -1,7 +1,6 @@
 <?php $heading = "Dashboard"; ?>
 <!-- Dashboard Component -->
 <?php require 'components/Admin/dashboard/sale-growth.php'; ?>
-<?php require 'components/Admin/dashboard/transaction.php'; ?>
 <?php require 'components/Admin/dashboard/order-history.php'; ?>
-<?php require 'components/Admin/dashboard/property.php'; ?>
+<?php require 'components/Admin/dashboard/transaction.php'; ?>
 <?php require 'components/Admin/dashboard/visited-country.php'; ?>
